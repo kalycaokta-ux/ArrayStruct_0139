44c97d31f42ffcfbe4248f2d59da4f5644ea4933
kalycaokta-ux <kalycaokta@gmail.com>
2026-04-07 18:41:36 +0700

    menambahkan data tampil

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -17,3 +17,16 @@ struct Orang
 int main ()
 {
     //deklarasi variabel struct
+    Orang mhs;
+    //Mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "Data Tampil" << endl;
