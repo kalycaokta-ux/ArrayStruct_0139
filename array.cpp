57dcdea4992c2bb54ef3a8af6f5d57b3cb635d0a
kalycaokta-ux <kalycaokta@gmail.com>
2026-04-07 17:22:30 +0700

    menambahkan isikan data pertama

--- array.cpp
+++ array.cpp
@@ -12,4 +12,8 @@ int main()
     cout << "Isi data kedua : " << dataku[1] << endl;
     //Mengisi array
     dataku[1] = 200;
+    cout << "Isi data kedua setelah diubah : " << dataku[1] << endl;
+
+    //Mengisi data dengan perintah satu persatu
+    cout << "Isikan data pertama : ";
 }
