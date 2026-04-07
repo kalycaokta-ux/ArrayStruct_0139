bd2e4248a0d0b57ce76e651c343478ee7d1c34f8
kalycaokta-ux <kalycaokta@gmail.com>
2026-04-07 17:19:40 +0700

    menambahkan isi data kedua

--- array.cpp
+++ array.cpp
@@ -7,3 +7,6 @@ int main()
     int dataku [5] = {10,20,30,40,50};
     //Deklarasi tanpa inisialisasi
     int datamu[5];
+
+    //Cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : " << dataku[1] << endl;
